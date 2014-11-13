f37a24bde26a8a93cd5a4e0597efa11f5d86531a
Alexandre <Alexandre@192.168.1.118>
2014-11-12 22:10:03 -0200

    navbar com os botoes prontos para login e registro, falta logout

---
+++ assets/php/navbar.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+  <div class="container">
+	<div class="navbar-header">
+	  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+		<span class="sr-only">Menu</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+	  </button>
+	</div>
+	<div id="navbar" class="navbar-collapse collapse">
+		<button type="button" id="login-btn" class="btn btn-primary" data-toggle="modal" data-target="#login-window">Login</button>
+		<button type="button" id="register-btn" class="btn btn-primary" data-toggle="modal" data-target="#register-window">Register</button>
+	</div><!--/.navbar-collapse -->
+  </div>
+</nav>

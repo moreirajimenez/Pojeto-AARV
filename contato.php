165f2935b489617599639aa5e5c523ee0d70c966
Arthur <Arthur@Arthur-PC>
2014-11-12 22:56:58 -0200

    php para contato com admin do site

---
+++ contato.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <?php
+        $title = 'Contato';
+        include './assets/php/head.php';
+    ?>
+	
+        <body>
+	    <?php 
+                include '/assets/php/conectaDB.php';
+            ?>
+            
+            <?php
+                include '/assets/php/navbar.php';
+            ?>
+            
+            <?php 
+                session_start();
+            ?>
+        
+            
+            
+            <div id="container">
+       
+            </div>    
+ 
+	</body>
+        
+</html>

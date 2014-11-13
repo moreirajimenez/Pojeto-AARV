5d07c64329e7a09f7bd5d7a69f56963de37693ba
Alexandre <Alexandre@192.168.1.118>
2014-11-12 22:09:19 -0200

    head com logo feito, css terminado, faltam meta tags. Adicionado script para SDK do facebook (falta a implementação)

---
+++ assets/php/head.php
@@ -0,0 +1,17 @@
+	<head>
+		<link rel="shortcut icon" type="image/x-icon" href="./assets/images/logo.png"/>
+		<meta charset="utf-8"/>
+		<meta name="descripition" content=""/>
+		<meta name="author" content=""/>
+
+		<title><?php echo $title ?></title>
+
+		<link rel="stylesheet" type="text/css" href="./assets/css/bootstrap.min.css"/>
+		<link rel="stylesheet" type="text/css" href="./assets/css/main.css"/>
+
+		<script type="text/javascript" src="./assets/js/JQuery.js"></script>
+		<script type="text/javascript" src="./assets/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="./assets/js/facebookSDK.js"></script>
+		<script type="text/javascript" src="./assets/js/validaCadastro.js"></script>
+
+	</head>

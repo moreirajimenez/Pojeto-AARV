a1fe2e18cd3ad34cb71dadb4c0a62a6f1f3ae7bb
Alexandre <Alexandre@192.168.1.118>
2014-11-20 23:31:05 -0200

    index terminada com conteudo e css.

--- index.php
+++ index.php
@@ -9,28 +9,31 @@
 
     <div class="container">
 	
-		<div class="img-container" id="logo-full-container">
-			<img id="logo-full" class="img-resize" src="./assets/img/full-logo.png">
+		<div class="img-container float-shadow" id="logo-full-container">
+			<img id="logo-full" class="img-resize float-shadow" src="./assets/img/full-logo.png">
 		</div>
 		
-		  <!-- Example row of columns -->
+		  <!-- Row -->
 		  <div class="row">
+		  
 			<div class="col-md-8">
-			  <h2>Heading</h2>
-			  <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-			</div>
+			  <h2>Sobre o Site</h2>
+			  <p>Aqui você encontrará informações e novidades sobre suas bandas e gêneros favoritos. Compartilhe suas experiências podendo discutir com outros usuários, poste suas fotos que tirou em shows, pesquise a agenda dos proximos shows e favorite as bandas que mais gostar! Aproveite e divirta-se com as informações aqui encontradas!
+			  </p>
+			</div><!--/col-md-8-->
 			
 			<div class="col-md-4">
-			  <h2>Heading</h2>
-			  <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-			  <p><a class="btn dtn-default" href="./home.php" role="button"><span class="glyphicon glyphicon-home"></span> Entrar</a></p>
-		   </div>
+			  <h2>Criação do Site</h2>
+			  <p>O site foi criado para o projeto de Tecnologia Web II da Universidade Presbiteriana Mackenzie, com o intuito de trazer conteudo musical. Criado por Arthur Ferreira, Alexandre Pucci, Vitor Coyado e Rodrigo Jimenez
+			  </p>
+			  <a class="btn btn-default" href="./home.php" role="button"><span class="glyphicon glyphicon-home"></span> Entrar</a>
+		   </div><!--/col-md-4-->
 		   
 		  </div><!--/row-->
 	
 		  <hr>
 	  
-    </div> <!-- /container -->
+    </div> <!--/container-->
 	
 	<?php
 		include "./assets/php/footer.php";

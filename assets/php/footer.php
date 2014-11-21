32f27a5045eda4d7f622268e503b5cbe867a752c
Alexandre <Alexandre@192.168.1.118>
2014-11-20 23:59:29 -0200

    footer terminado

--- assets/php/footer.php
+++ assets/php/footer.php
@@ -1,3 +1,6 @@
-<footer>
-        <p>&copy; Alexandre Pucci, Vitor Coyado, Arthur Ferreira e Rodrigo Jimenez</p>
-</footer>
+<div class="container-footer">
+	<hr>
+	<footer>
+			<p>&copy; 2014 TecWeb II - Arthur Ferreira, Alexandre Pucci, Vitor Coyado e Rodrigo Jimenez</p>
+	</footer>
+</div><!--/container-footer-->

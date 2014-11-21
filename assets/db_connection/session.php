80111d9c27e59d99aa153a8d4b4a7fe0c8a8f17d
Alexandre <Alexandre@192.168.1.118>
2014-11-20 23:50:28 -0200

    session.

---
+++ assets/db_connection/session.php
@@ -0,0 +1,11 @@
+<?php 
+	include "./db_connection/db.php";
+	
+	session_start();
+	if(isset($_SESSION['session_user'])){
+		
+		$user=$_SESSION['session_user'];
+		$session_sql=mysql_query("SELECT USERNAME FROM USUARIOS WHERE USERNAME='".$user."'", $connection);
+		$row = mysql_fetch_assoc($session_sql);	
+	}
+?>

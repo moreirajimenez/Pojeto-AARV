127c10bf9d59c984b6e6d6b1bb1c3a58768088b6
Alexandre <Alexandre@192.168.1.118>
2014-11-20 23:32:15 -0200

    home otimizada com uso de includes. Finalizada.

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <?php 
 	$title = "Music Beast - Home";
 	include "./assets/php/head.php";
@@ -18,65 +18,24 @@
 		</div>
 	
 	</div><!--/container-->
-
-	<div class="col-m6" id="sidebar" role="navigation">
-		<div class="list-group">
-			<a href="./hard-rock.php" class="list-group-item">Hard Rock</a>
-			<a href="./pop.php" class="list-group-item">Pop</a>
-			<a href="./indie.php" class="list-group-item">Indie</a>
-			<a href="./eletronica.php" class="list-group-item">Eletrônica</a>
-			<a href="./rap.php" class="list-group-item">Rap</a>
-		</div><!--/list-group-->
-	</div><!--/sidebar-->
 	
-	<div class="container-footer">
-		<hr>
+	 <?php 
+		include "./assets/php/sidebar.php";
+	 ?>
+	
+	<div class="col-md-10" class="conteudo">
 		<?php
-			include "./assets/php/footer.php";
+			include "./db_connection/db.php";
+			$genero = null;
+			include "./db_connection/listar-posts.php";
 		?>
-		
-	</div><!--/container-->
+	</div><!--conteudo-->
 	
-	<div id="login-window" class="modal fade login-window" tabindex="-1" role="dialog" aria-labelledby="login-window-label" aria-hidden="true">
-		<div class="modal-dialog modal-sm">
-			<div class="modal-content">
-				<h2 id="login-title">Login</h2>
-				<form action="./assets/php/login.php" method="post">
-					<label>Username:</label>
-					<input id="name" name="username" placeholder="username" type="text">
-					
-					<label>Password:</label>
-					<input id="password" name="password" placeholder="**********" type="password">
-					
-					<input class="submit-btn" name="submit" type="submit" value="Login">
-				</form>
-			</div><!--modal-content-->
-		</div><!--modal-dialog-->
-	</div><!--modal-->
-	
-	<div id="register-window" class="modal fade login-window" tabindex="-1" role="dialog" aria-labelledby="register-window-label" aria-hidden="true">
-		<div class="modal-dialog modal-sm">
-			<div class="modal-content">
-				<h2 id="login-title">Registro de Usuário</h2>
-				<form action="./assets/php/register.php" method="post">
-					<label>Nome:</label>
-					<input id="nome" name="Nome" placeholder="Nome" type="text">
-					
-					<label>E-Mail:</label>
-					<input id="email" name="E-mail" placeholder="E-Mail" type="text">
-					
-					<label>Username:</label>
-					<input id="name" name="username" placeholder="username" type="text">
-					
-					<label>Password:</label>
-					<input id="password" name="password" placeholder="**********" type="password">
-					
-					<input class="submit-btn" name="submit" type="submit" value="Registrar">
-				</form>
-	
-			</div><!--modal-content-->
-		</div><!--modal-dialog-->
-	</div><!--modal-->
-	
+	<!--footer-->
+	<?php
+		include "./assets/php/footer.php";
+		include "./assets/php/modal.php";
+	?>	
+
   </body>
 </html>

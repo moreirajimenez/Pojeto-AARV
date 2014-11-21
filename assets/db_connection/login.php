98898d108217923ab874315e0bfe4e6a9d1eb177
Alexandre <Alexandre@192.168.1.118>
2014-11-20 23:49:04 -0200

    login com DB funcionando

---
+++ assets/db_connection/login.php
@@ -0,0 +1,27 @@
+<?php
+	include "./db.php";
+	
+	session_start();
+	
+	$username=$_POST['username'];
+	$password=$_POST['password'];
+	
+	$username = stripslashes($username);
+	$password = stripslashes($password);
+	$username = mysql_real_escape_string($username);
+	$password = mysql_real_escape_string($password);
+	
+	$query = "SELECT * FROM USUARIOS WHERE PASSWORD='".$password."' AND USERNAME='".$username."'";
+
+	$result = mysql_query($query);
+	$rows = mysql_num_rows($result);
+	
+	if ($rows == 1) {
+		$_SESSION['session_user']=$username;
+		$_SESSION['session_password']=$password;
+		header("location: ../profile.php");
+	} else {
+		//Login Invalido
+		header("location: ../login-error.php");
+	}
+?>

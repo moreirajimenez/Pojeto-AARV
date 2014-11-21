9c5155a883686c9d815f5c3fbf82db15a5dde65b
Alexandre <Alexandre@192.168.1.118>
2014-11-20 23:30:08 -0200

    pagina pop linkada ja com banco de dados de noticias

---
+++ pop.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<?php 
+	$title = "Music Beast - Pop";
+	include "./assets/php/head.php";
+?>
+
+  <body>
+  
+	 <?php 
+		include "./assets/php/navbar.php";
+	 ?>
+
+    <div class="container">
+		
+		<div class="img-container" id="logo-container">
+			<img id="logo" class="img-resize img-circle" src="./assets/img/logo.png">
+		</div>
+	
+	</div><!--/container-->
+	
+	 <?php 
+		include "./assets/php/sidebar.php";
+	 ?>
+	
+	<div class="col-md-10" class="conteudo">
+		<?php
+			include "./db_connection/db.php";
+			$genero = "POP";
+			include "./db_connection/listar-posts.php";
+		?>
+	</div><!--conteudo-->
+	
+	<!--footer-->
+	<?php
+		include "./assets/php/footer.php";
+		include "./assets/php/modal.php";
+	?>	
+
+  </body>
+</html>

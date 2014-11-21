a6fa4669161d307e9d956bf582cb8d1427a0719c
Alexandre <Alexandre@192.168.1.118>
2014-11-20 23:59:20 -0200

    head com meta tags. completo.

--- assets/php/head.php
+++ assets/php/head.php
@@ -1,12 +1,14 @@
 	<head>
 		<link rel="shortcut icon" type="image/x-icon" href="./assets/images/logo.png"/>
 		<meta charset="utf-8"/>
-		<meta name="descripition" content=""/>
-		<meta name="author" content=""/>
+		<meta name="descripition" content="Site para acesso a noticias de diversos generos de musica."/>
+		<meta name="author" content="Alexandre Pucci, Arthur Ferreira, Vitor Coyado, Rodrigo Jimenez"/>
+		<meta name="keywords" content="musica,rap,rock,hardrock,hard,Music,Beast,MusicBeast,fotos,shows,noticias,music,news">
 
 		<title><?php echo $title ?></title>
 
 		<link rel="stylesheet" type="text/css" href="./assets/css/bootstrap.min.css"/>
+		<link rel="stylesheet" type="text/css" href="./assets/css/hover-min.css"/>
 		<link rel="stylesheet" type="text/css" href="./assets/css/main.css"/>
 
 		<script type="text/javascript" src="./assets/js/JQuery.js"></script>
